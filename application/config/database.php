cfb7335cb236f4239e82d546fa36f3573d7dc89d
Eamce <eamsanroel19@gmail.com>
2023-05-21 22:18:36 +0800

    sdddd

--- application/config/database.php
+++ application/config/database.php
@@ -215,6 +215,32 @@ $db['default'] = array(
 
 // $db['default'] = array(
 // 	'dsn'	=> '',
+// 	'hostname' => 'localhost',
+// 	'port' => '',
+// 	'username' => 'root',
+// 	'password' => '',
+// 	'database' => 'alturush',
+// 	//'database' => 'fe_commerce2',
+// 	'dbdriver' => 'mysqli',
+// 	'dbprefix' => '',
+// 	'pconnect' => FALSE,
+// 	'db_debug' => (ENVIRONMENT !== 'production'),
+// 	'cache_on' => FALSE,
+// 	'cachedir' => '',
+// 	'char_set' => 'utf8',
+// 	'dbcollat' => 'utf8_general_ci',
+// 	'swap_pre' => '',
+// 	'encrypt' => FALSE,
+// 	'compress' => FALSE,
+// 	'stricton' => FALSE,
+// 	'failover' => array(),
+// 	'save_queries' => TRUE
+// );
+
+
+
+// $db['default'] = array(
+// 	'dsn'	=> '',
 // 	'hostname' => '172.16.43.27',
 // 	'port' => '',
 // 	'username' => 'alturush',
